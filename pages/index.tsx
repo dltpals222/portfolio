fd9bbb8556813b416caa85189b0cc3cc63f5c73e
leesemin <leesemin222@gmail.com>
2023-07-20 17:31:41 +0900

    이미지 붙이기가 생각보다 오래걸릴거 같아서 패스

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
 import React, { useState } from "react";
 
 const StartImg = () => {
-  return <div></div>;
+  return (
+    <div className="w-screen h-screen grid grid-rows-2 grid-cols-2 gap-2">
+      <Image src="/img/Rectangle-first.png" alt="첫번째 블록" width={606.06} height={378.79} />
+      <Image src="/img/Rectangle-second.png" alt="두번째 블록" width={606.06} height={378.79} />
+      <Image src="/img/Rectangle-fourth.png" alt="네번째 블록" width={378.79} height={606.06} />
+      <Image src="/img/Rectangle-thrid.png" alt="세번째 블록" width={378.79} height={606.06} />
+    </div>
+  );
 };
 
 export default StartImg;
