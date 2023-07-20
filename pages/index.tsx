df2b2a4807146be244da35db054b4c7815001e23
leesemin <leesemin222@gmail.com>
2023-07-20 18:29:51 +0900

    2겹으로 해서 클릭이벤트 만들어보기

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,26 @@ import React, { useState } from "react";
 const StartImg = () => {
   return (
     <div className="w-screen h-screen flex justify-center items-center relative">
-      <div className="z-1 w-[825px] h-[825px] grid grid-rows-2 grid-cols-2 gap-[25px]"></div>
-      <div className="absolute z-0 w-[825px] h-[825px] grid grid-rows-2 grid-cols-2 gap-[25px]">
+      <div className="z-0 w-[825px] h-[825px] grid grid-rows-2 grid-cols-2 gap-[25px]">
+        <div>
+          <div className="w-[400px] h-[125px]" />
+          <div className="w-[187.5px] h-[125px]" />
+          <div className="w-[187.5px] h-[125px]" />
+          <div className="w-[400px] h-[125px]" />
+        </div>
+        <div>
+          <div className="w-[187.5px] h-[125px]" />
+          <div className="w-[400px] h-[125px]" />
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className="absolute z-1 w-[825px] h-[825px] grid grid-rows-2 grid-cols-2 gap-[25px]">
         <Image src="/img/Group-71.png" alt="첫번째 블록" width={400} height={400} />
         <Image src="/img/Group-72.png" alt="두번째 블록" width={400} height={400} />
-        <Image src="/img/Group-73.png" alt="네번째 블록" width={400} height={400} />
-        <Image src="/img/Group-74.png" alt="세번째 블록" width={400} height={400} />
+        <Image src="/img/Group-73.png" alt="세번째 블록" width={400} height={400} />
+        <Image src="/img/Group-74.png" alt="네번째 블록" width={400} height={400} />
       </div>
     </div>
   );
