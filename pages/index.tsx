4d9a5c1b395c4c00e7c1a6ae93ab9797f7a8c1a2
leesemin <leesemin222@gmail.com>
2023-07-20 17:36:46 +0900

    이미지 크기 줄이고 다시 저장

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,10 @@ import React, { useState } from "react";
 const StartImg = () => {
   return (
     <div className="w-screen h-screen grid grid-rows-2 grid-cols-2 gap-2">
-      <Image src="/img/Rectangle-first.png" alt="첫번째 블록" width={606.06} height={378.79} />
-      <Image src="/img/Rectangle-second.png" alt="두번째 블록" width={606.06} height={378.79} />
-      <Image src="/img/Rectangle-fourth.png" alt="네번째 블록" width={378.79} height={606.06} />
-      <Image src="/img/Rectangle-thrid.png" alt="세번째 블록" width={378.79} height={606.06} />
+      <Image src="/img/Rectangle-first-second.png" alt="첫번째 블록" width={400} height={400} />
+      <Image src="/img/Rectangle-thrid-fourth.png" alt="두번째 블록" width={400} height={400} />
+      <Image src="/img/Rectangle-thrid-fourth.png" alt="네번째 블록" width={400} height={400} />
+      <Image src="/img/Rectangle-first-second.png" alt="세번째 블록" width={400} height={400} />
     </div>
   );
 };
