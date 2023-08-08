ddf8a5d6d93be705ae3e6a592ac50202cfc9d0ee
leesemin <leesemin222@gmail.com>
2023-08-09 04:48:14 +0900

    작동은 되는데 애니메이션이 안나옴

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,24 +3,28 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
-const AnimatedPageTransition = () => {
+const About = () => {
   const router = useRouter();
 
   const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
-    router.push("/about");
+    router.push("/");
   };
 
   return (
     <TransitionGroup component={null}>
       <CSSTransition classNames="page-transition" timeout={1200}>
         <main>
-          <div className="cd-index cd-main-content">
+          <div className="cd-about cd-main-content">
             <div>
-              <h1>Animated Page Transition</h1>
-              <Link href="/about" passHref legacyBehavior>
+              <h1>About Page</h1>
+              <p>
+                This is the about page with the animated page transition. You can put any content
+                you want here.
+              </p>
+              <Link href="/" passHref legacyBehavior>
                 <a className="cd-btn" data-type="page-transition" onClick={handleClick}>
-                  Start animation
+                  Go Back
                 </a>
               </Link>
             </div>
@@ -31,4 +35,4 @@ const AnimatedPageTransition = () => {
   );
 };
 
-export default AnimatedPageTransition;
+export default About;
