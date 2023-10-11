cfd5187bf0d1e3a0f348bef965c5ce6dca2fa099
leesemin <leesemin222@gmail.com>
2023-10-11 23:22:10 +0900

    메인페이지 CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,45 +30,82 @@ const Index: React.FC = () => {
     };
   }, [router]);
 
-  const publicClass: string = "w-full w-50vh";
+  const publicClass: string = "w-full w-50vh relative flex";
+  const ImageClass: string = "absolute";
+  const ImageLarge = {
+    first: 400,
+    second: 400,
+  };
 
   return (
     <div>
       <div id="img-cnt">
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-          <div className={`pl-4 pt-4 ${publicClass}`}>
+          <div className={`pl-4 pt-4 ${publicClass} flex-col`}>
             <Image
-              src="/img/Rectangle-Group-1.png"
+              src="/img/Rectangle-first.png"
               alt="첫번째 블록"
-              width={400}
-              height={400}
+              width={ImageLarge.first}
+              height={ImageLarge.second}
+              layout="responsive"
+            />
+            <Image
+              className={`${ImageClass} top-[25%] pr-4`}
+              src="/img/Rectangle-second.png"
+              alt="두번째 블록"
+              width={ImageLarge.first}
+              height={ImageLarge.second}
               layout="responsive"
             />
           </div>
           <div className={`pr-4 pt-4 ${publicClass}`}>
             <Image
-              src="/img/Rectangle-Group-2.png"
-              alt="두번째 블록"
-              width={400}
-              height={400}
+              src="/img/Rectangle-third.png"
+              alt="세번째 블록"
+              width={ImageLarge.second}
+              height={ImageLarge.first}
+              layout="responsive"
+            />
+            <Image
+              className={`${ImageClass} left-[60%]`}
+              src="/img/Rectangle-fourth.png"
+              alt="네번째 블록"
+              width={ImageLarge.second}
+              height={ImageLarge.first}
               layout="responsive"
             />
           </div>
           <div className={`pl-4 pb-4 ${publicClass}`}>
             <Image
-              src="/img/Rectangle-Group-3.png"
-              alt="세번째 블록"
-              width={400}
-              height={400}
+              src="/img/Rectangle-fifth.png"
+              alt="다섯번째 블록"
+              width={ImageLarge.second}
+              height={ImageLarge.first}
+              layout="responsive"
+            />
+            <Image
+              className={`${ImageClass} left-[60%]`}
+              src="/img/Rectangle-sixth.png"
+              alt="여섯번째 블록"
+              width={ImageLarge.second}
+              height={ImageLarge.first}
               layout="responsive"
             />
           </div>
-          <div className={`pr-4 pb-4 ${publicClass}`}>
+          <div className={`pr-4 pb-4 ${publicClass} flex-col`}>
             <Image
-              src="/img/Rectangle-Group-4.png"
-              alt="네번째 블록"
-              width={400}
-              height={400}
+              src="/img/Rectangle-seventh.png"
+              alt="일곱번째 블록"
+              width={ImageLarge.second}
+              height={ImageLarge.first}
+              layout="responsive"
+            />
+            <Image
+              className={`${ImageClass} top-[25%] pr-4`}
+              src="/img/Rectangle-eighth.png"
+              alt="여덟번째 블록"
+              width={ImageLarge.second}
+              height={ImageLarge.first}
               layout="responsive"
             />
           </div>
