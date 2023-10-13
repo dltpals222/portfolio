f6cf0265447bc0d82aa697d19188fc6ed3c6c7e9
leesemin <leesemin222@gmail.com>
2023-10-13 20:30:25 +0900

    배경이 될 도형 컴포넌트로 만들기

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import Image from "next/image";
 
 const Index: React.FC = () => {
   const router = useRouter();
@@ -39,78 +38,7 @@ const Index: React.FC = () => {
 
   return (
     <div>
-      <div id="img-cnt">
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-          <div className={`pl-4 pt-4 ${publicClass} flex-col`}>
-            <Image
-              src="/img/Rectangle-first.png"
-              alt="첫번째 블록"
-              width={ImageLarge.first}
-              height={ImageLarge.second}
-              layout="responsive"
-            />
-            <Image
-              className={`${ImageClass} top-[25%] pr-4`}
-              src="/img/Rectangle-second.png"
-              alt="두번째 블록"
-              width={ImageLarge.first}
-              height={ImageLarge.second}
-              layout="responsive"
-            />
-          </div>
-          <div className={`pr-4 pt-4 ${publicClass}`}>
-            <Image
-              src="/img/Rectangle-third.png"
-              alt="세번째 블록"
-              width={ImageLarge.second}
-              height={ImageLarge.first}
-              layout="responsive"
-            />
-            <Image
-              className={`${ImageClass} left-[60%]`}
-              src="/img/Rectangle-fourth.png"
-              alt="네번째 블록"
-              width={ImageLarge.second}
-              height={ImageLarge.first}
-              layout="responsive"
-            />
-          </div>
-          <div className={`pl-4 pb-4 ${publicClass}`}>
-            <Image
-              src="/img/Rectangle-fifth.png"
-              alt="다섯번째 블록"
-              width={ImageLarge.second}
-              height={ImageLarge.first}
-              layout="responsive"
-            />
-            <Image
-              className={`${ImageClass} left-[60%]`}
-              src="/img/Rectangle-sixth.png"
-              alt="여섯번째 블록"
-              width={ImageLarge.second}
-              height={ImageLarge.first}
-              layout="responsive"
-            />
-          </div>
-          <div className={`pr-4 pb-4 ${publicClass} flex-col`}>
-            <Image
-              src="/img/Rectangle-seventh.png"
-              alt="일곱번째 블록"
-              width={ImageLarge.second}
-              height={ImageLarge.first}
-              layout="responsive"
-            />
-            <Image
-              className={`${ImageClass} top-[25%] pr-4`}
-              src="/img/Rectangle-eighth.png"
-              alt="여덟번째 블록"
-              width={ImageLarge.second}
-              height={ImageLarge.first}
-              layout="responsive"
-            />
-          </div>
-        </div>
-      </div>
+      
     </div>
   );
 };
