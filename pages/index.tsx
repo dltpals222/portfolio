f661ef889b69e874ab8aa15f44b8aec209b09bba
leesemin <leesemin222@gmail.com>
2023-10-22 05:54:07 +0900

    얼추 되는 느낌이 든다

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import React from "react";
+import Rectangle from "./main/rectangle";
 
 const Index: React.FC = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY;
-      const windowHeight = window.innerHeight;
-      const docHeight = document.documentElement.scrollHeight;
-      console.log({
-        scrollPosition: scrollPosition,
-        windowHeight: windowHeight,
-        docHeight: docHeight,
-      });
-
-      // 아래로 스크롤시 (200px)
-      if (docHeight - (scrollPosition + windowHeight) < 200) {
-        router.push("nav.tsx");
-      }
-
-      window.addEventListener("scroll", handleScroll);
-
-      // 컴포넌트 unmounts시 이벤트 제거
-      return () => {
-        window.removeEventListener("scroll", handleScroll);
-      };
-    };
-  }, [router]);
-
-  const publicClass: string = "w-full w-50vh relative flex";
-  const ImageClass: string = "absolute";
-  const ImageLarge = {
-    first: 400,
-    second: 400,
-  };
-
   return (
     <div>
-      
+      <Rectangle></Rectangle>
     </div>
   );
 };
--- pages/main/rectangle.tsx
+++ pages/main/rectangle.tsx
@@ -1,105 +1,55 @@
-import React from 'react';
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
 const Rectangle: React.FC = () => {
-  const publicClass: string = "w-full w-50vh relative flex";
+  const publicClass: string = "relative flex w-[400px] h-[400px] flex-col";
+  const divImgClass = "absolute top-0 left-0 overflow-hidden w-full";
   const ImageClass: string = "absolute";
   const ImageLarge = {
     first: 400,
     second: 400,
   };
-  
-  return (
-  <div>
-    <div id="img-cnt">
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-        <div className={`pl-4 pt-4 ${publicClass} flex-col`}>
-          <div>
-            <Image
-              src="/img/Rectangle-first.png"
-              alt="첫번째 블록"
-              width={ImageLarge.first}
-              height={ImageLarge.second}
-              layout="responsive"
-              />
-          </div>
-          <div>
-            <Image
-              className={`${ImageClass} top-[25%] pr-4`}
-              src="/img/Rectangle-second.png"
-              alt="두번째 블록"
-              width={ImageLarge.first}
-              height={ImageLarge.second}
-              layout="responsive"
-              />
-          </div>
-        </div>
-        <div className={`pr-4 pt-4 ${publicClass}`}>
-          <div>
-            <Image
-              src="/img/Rectangle-third.png"
-              alt="세번째 블록"
-              width={ImageLarge.second}
-              height={ImageLarge.first}
-              layout="responsive"
-              />
-          </div>
-          <div>
-            <Image
-              className={`${ImageClass} left-[60%]`}
-              src="/img/Rectangle-fourth.png"
-              alt="네번째 블록"
-              width={ImageLarge.second}
-              height={ImageLarge.first}
-              layout="responsive"
-            />
-          </div>
-        </div>
-        <div className={`pl-4 pb-4 ${publicClass}`}>
-          <div>
-            <Image
-              src="/img/Rectangle-fifth.png"
-              alt="다섯번째 블록"
-              width={ImageLarge.second}
-              height={ImageLarge.first}
-              layout="responsive"
-              />
-          </div>
-          <div>
-            <Image
-              className={`${ImageClass} left-[60%]`}
-              src="/img/Rectangle-sixth.png"
-              alt="여섯번째 블록"
-              width={ImageLarge.second}
-              height={ImageLarge.first}
-              layout="responsive"
-            />
-          </div>
-        </div>
-        <div className={`pr-4 pb-4 ${publicClass} flex-col`}>
-          <div>
 
-            <Image
-              src="/img/Rectangle-seventh.png"
-              alt="일곱번째 블록"
-              width={ImageLarge.second}
-              height={ImageLarge.first}
-              layout="responsive"
-            />
+  return (
+    <div>
+      <div id="img-cnt">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+          <div className={`${publicClass}`}>
+            <div style={{ height: "initial" }} className={divImgClass}>
+              <Image src="/img/Rectangle-first.png" alt="첫번째 블록" fill={true} />
+            </div>
+            <div style={{ top: "37%" }} className={divImgClass}>
+              <Image src="/img/Rectangle-second.png" alt="두번째 블록" fill={true} />
+            </div>
           </div>
-          <div>
-            <Image
-              className={`${ImageClass} top-[25%] pr-4`}
-              src="/img/Rectangle-eighth.png"
-              alt="여덟번째 블록"
-              width={ImageLarge.second}
-              height={ImageLarge.first}
-              layout="responsive"
-            />
+          <div className={`${publicClass}`}>
+            <div className={divImgClass}>
+              <Image src="/img/Rectangle-third.png" alt="세번째 블록" fill={true} />
+            </div>
+            <div style={{ left: "37%" }} className={divImgClass}>
+              <Image src="/img/Rectangle-fourth.png" alt="네번째 블록" fill={true} />
+            </div>
+          </div>
+          <div className={`${publicClass}`}>
+            <div className={divImgClass}>
+              <Image src="/img/Rectangle-fifth.png" alt="다섯번째 블록" fill={true} />
+            </div>
+            <div style={{ left: "37%" }} className={divImgClass}>
+              <Image src="/img/Rectangle-sixth.png" alt="여섯번째 블록" fill={true} />
+            </div>
+          </div>
+          <div className={`${publicClass}`}>
+            <div className={divImgClass}>
+              <Image src="/img/Rectangle-seventh.png" alt="일곱번째 블록" fill={true} />
+            </div>
+            <div style={{ top: "37%" }} className={divImgClass}>
+              <Image src="/img/Rectangle-eighth.png" alt="여덟번째 블록" fill={true} />
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
+
+export default Rectangle;
