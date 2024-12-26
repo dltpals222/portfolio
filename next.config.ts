9957d2e6de622a16fcc6cf9c0fadbac7828c8293
이세민 <leesemin@isemin-ui-MacBookPro.local>
2024-12-25 22:14:08 -0800

    모든 경로 원복

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dltpals222.github.io',
+        pathname: '/portfolio/**',
+      },
+    ],
+    domains: ['dltpals222.github.io'],
   },
   basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? 'https://dltpals222.github.io/portfolio' : '',
