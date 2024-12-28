81ac364008ee49f127d09bdad6ed487706ed08b2
이세민 <leesemin@isemin-ui-MacBookPro.local>
2024-12-28 00:30:27 -0800

    저장

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -4,7 +4,7 @@ export default function About() {
   const profile = {
     name: "이세민",
     birth: "1992.07",
-    phone: "010-7***-8***",
+    phone: "010-7***`-8***",
     location: "대전광역시",
     education: "4년제 대학교 졸업",
     github: "https://github.com/dltpals222",
